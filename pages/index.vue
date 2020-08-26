e8a817b19fa84aa3c8409d30eefe3ee013c60254
jabelic <hibiki990523@gmail.com>
2020-08-26 14:29:43 +0900

    _slug

--- pages/index.vue
+++ pages/index.vue
@@ -24,11 +24,27 @@
         </a>
       </div>
     </div>
+    <li v-for="post of posts" :key="post.slug">
+      <NuxtLink :to="post.slug">{{ post.title }}</NuxtLink>
+    </li>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    };
+  },
+  async asyncData({ $content }) {
+    const posts = await $content("blog").fetch();
+
+    return {
+      posts,
+    };
+  },
+};
 </script>
 
 <style>
