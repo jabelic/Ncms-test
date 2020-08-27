6d2ed7492eca6a47697fd9dc46c027ddd2bc5bea
jabelic <hibiki990523@gmail.com>
2020-08-28 01:27:16 +0900

    add all

--- pages/index.vue
+++ pages/index.vue
@@ -34,12 +34,7 @@
 </template>
 
 <script>
-//import navbar from '@/components/Navbar';
-
 export default {
-  //components:{
-    //'navbar': navbar,
-  //},
   head() {
     return {
       //script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
